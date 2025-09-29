bf2ebd0e0cd5ecea461c9f48c29f297107de9c16
Patrick <patrick@gotelco.com.au>
2025-09-29 15:46:32 +1000

    Added more stuff to update.sql

--- Update_43.sql
+++ Update_43.sql
@@ -22,3 +22,19 @@ FROM `partner`
 WHERE Name = 'Kakadu Kitchen';
 
 -- Task 2: 
+-- 1. Insert the new partnership (independent Kakadu Kitchen production)
+INSERT INTO partnership (StartDate, TotalInvestment)
+VALUES ('2025-09-01', 0.00);
+
+-- 2. Link Kakadu Kitchen to this new partnership
+INSERT INTO partnerpartnership (PartnerID, PartnershipID, PartnerInvShare, PartnerSaleShare)
+SELECT PartnerID, LAST_INSERT_ID(), 0.00, 100.00
+FROM partner
+WHERE Name = 'Kakadu Kitchen';
+
+-- 3. Insert the four product variations into this new Kakadu Kitchen-only partnership
+INSERT INTO partnershipproduct (ProductNo, PartnershipID, TotalUnitAgreed, TotalUnitDelivered)
+SELECT ProductNo, LAST_INSERT_ID(), 5000, 0
+FROM product
+WHERE ProductNo IN (21, 22, 33, 34);
+
